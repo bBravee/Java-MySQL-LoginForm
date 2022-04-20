d6e7385fc3e9600d256391ce6220c332f5e1f324
bBravee <101563516+bBravee@users.noreply.github.com>
2022-04-20 18:18:47 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -7,4 +7,4 @@ INSERT INTO your_schema_name.your_table_name VALUES
 (NULL, 'Zenobia Night', 'zenobia@gmail.com', '+987654321', 'Russia, Moscow', '65423'),  
 (NULL, 'Juliet Boris', 'juliet@gmail.com', '+872635198', 'France, Paris', '91274'),
 (NULL, 'Issa Giovanni', 'issag@gmail.com', '+518765234', 'Italy, Rome', '00032'),
-(NULL, 'Tara Canmore', 'canmore@gmail.com', '+998887665', 'Spain, Madrit', '99911');
+(NULL, 'Tara Canmore', 'canmore@gmail.com', '+998887665', 'Spain, Madrid', '99911');
